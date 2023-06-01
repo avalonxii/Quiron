ba42ec0025aa64cfdab4ec19f75170fdb8096164
avalon <juanmarcial.portilla@campusfp.es>
2023-06-01 20:09:04 +0200

    fix: login user no exist

--- src/routes/login/+page.server.ts
+++ src/routes/login/+page.server.ts
@@ -12,27 +12,30 @@ export const actions = {
         const passw = formData.get('password');
 
         const user: User = await fetch(`/api/users?email=${email}`, { method: 'GET' }).then(result => result.json())
-        const { password } = user;
+        
+        if(user){
+            const { password } = user;
 
-        const passwordMatch = await verifyPassword( passw as string, password);
-
-        if (passwordMatch) {
-            //create cookie
-            cookies.set('auth', 'regularusertoken', {
-                // send cookie for every page
-                path: '/',
-                // server side only cookie so you can't use `document.cookie`
-                httpOnly: true,
-                // only requests from same site can send cookies
-                // https://developer.mozilla.org/en-US/docs/Glossary/CSRF
-                sameSite: 'strict',
-                // only sent over HTTPS in production
-                secure: process.env.NODE_ENV === 'production',
-                maxAge: 60 * 60 * 24 * 30, //1 month
-            })
-
-            // redirect the user
-            throw redirect(302, '/')
+            const passwordMatch = await verifyPassword( passw as string, password);
+    
+            if (passwordMatch) {
+                //create cookie
+                cookies.set('auth', 'regularusertoken', {
+                    // send cookie for every page
+                    path: '/',
+                    // server side only cookie so you can't use `document.cookie`
+                    httpOnly: true,
+                    // only requests from same site can send cookies
+                    // https://developer.mozilla.org/en-US/docs/Glossary/CSRF
+                    sameSite: 'strict',
+                    // only sent over HTTPS in production
+                    secure: process.env.NODE_ENV === 'production',
+                    maxAge: 60 * 60 * 24 * 30, //1 month
+                })
+    
+                // redirect the user
+                throw redirect(302, '/')
+            }
         }
 
         return{
